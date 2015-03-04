ccbb2461b7f7aba5f3d9aaf7b2b3e35e9fa6d899
Tyler Cecchi <thcecchi@gmail.com>
2015-03-04 18:11:26 -0500

    added custom template directives

---
+++ app/common/productDirective.js
@@ -0,0 +1,27 @@
+(function () {
+  "use strict";
+  angular.module("day33Hw")
+    .directive("prodDirective", function () {
+
+      return {
+        restrict: 'E', ///E = element, A = attribute, C = class
+        templateUrl: 'common/views/productDirective.html',
+        transclude: false,
+        scope: {
+          name: '@', // @ = data can come in, = means that data is 2-way, & = express or function
+          photo: '@',
+          price: '@',
+          qty: '@'
+        }
+        // link: function (scope, element, attributes) {
+        //   console.log(scope)
+        //   element.on('click', function (e) {
+        //     e.preventDefault
+        //     element.css('color','tomato')
+        //     element.append('<h2>Sweet!</h2>')
+        //   })
+        // }
+      };
+
+  });
+})();
